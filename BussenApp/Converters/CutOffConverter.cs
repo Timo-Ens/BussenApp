71391567c4f8b73c131a2d12a6ce456c2fec4496
TimoEnserink <tenserink@student.scalda.nl>
2024-09-18 23:03:58 +0200

    Change layout

---
+++ BussenApp/Converters/CutOffConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussenApp.Converters
+{
+    public class CutoffConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value) > Cutoff;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Cutoff { get; set; }
+    }
+}
